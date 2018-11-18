f0e638b2abd7657a3019b5007568da2a6237b412
Brett Spangler <bns5273@psu.edu>
2018-11-18 00:17:47 -0500

    much faster input processing

--- 10-5-2018.py
+++ 10-5-2018.py
@@ -1,53 +1,68 @@
+"""
+TODO:
+generators
+black as negative will not work!
+dimensionality reduction
+automated rating estimation using stockfish binary?
+"""
+
 import chess
 import chess.pgn as pgn
 import chess.uci
 from functions import *
 import re
+from numpy import mean, corrcoef
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
 
-if __name__ == '__main__':
-
-    stockfish = chess.uci.popen_engine('stockfish_9_x64.exe')
-    stockfish.uci()
-
-    file = open('ccrl_stockfish.pgn', 'r', encoding='Latin-1')
+def mygenerator(filename):
+    file = open(filename, 'r', encoding='Latin-1')
 
     position = 0
     game = 0
     for offset, header in pgn.scan_headers(file):
         white = header['White']
         black = header['Black']
-        result = header['Result']
+        result = get_result(header)
 
         move = 1
         x = []
         y = []
-        # b_eval = []
-        node = pgn.read_game(file)
-        while not node.is_end():
-            next_node = node.variations[0]
-            evaluation = re.findall('[-+]?\d*\.\d+', next_node.comment)
-            if evaluation:          # evaluation is empty for mating and book opening moves
-                if move % 1 == 0:   # only white to move positions. some engines (as black) evaluate from perspective!
-                    x.append(get_x(next_node))
-                    y.append(winning_chances(float(evaluation[0])))
+        old_node = pgn.read_game(file)
+        while not old_node.is_end():
+            node = old_node.variations[0]
+            eval = re.findall('[-+]?\d*\.\d+', node.comment)
+            # evaluation is empty for mating and book opening moves
+            if eval:
+                # only white to move positions. some engines (as black) evaluate from perspective!
+                if move % 1 == 0:
+                    yield get_x(node), float(eval[0])
                     position += 1
-
-                print(next_node.board().fen(), y[-1])
-            node = next_node
+            old_node = node
             move += .5
-        # print(g, result, '\n')
         game += 1
 
-        if game == 1:
+        if game == 10:
             break
 
-    print(game)
-    print(position)
+        if game % 1000 == 0:
+            print(game / 916663)
+
+    print(game, 'games', position, 'positions')
+
+
+if __name__ == '__main__':
+
+    # stockfish = chess.uci.popen_engine('/usr/bin/stockfish')
+    # stockfish.uci()
+
+    for x, y in mygenerator('sources/ccrl_stockfish.pgn'):
+        print(y)
+
+
 
     # plt.plot(range(len(w_eval)), w_eval)
     # plt.plot(range(len(b_eval)), b_eval)
@@ -55,5 +70,4 @@ if __name__ == '__main__':
     # plt.ylim(-10, 10)
     # plt.show()
 
-    if game % 1000 == 0:
-        print(game / 916663)
+
--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import pprint
 import numpy as np
 
 
-def result_bool(h):
+def get_result(h):
     if h['Result'] == '1-0':
         return 1
     elif h['Result'] == '0-1':
@@ -22,53 +22,39 @@ def winning_chances(centipawns):
     return 2 / (1 + math.exp(-0.004 * centipawns)) - 1
 
 
+# this converts an integer to a bit array for qualitative tensors
+# pretty fast
+def to_bin_array(p, w, b):
+    # convert integers to length 64 strings. mask pieces by team
+    w = bin(w & p)[2:]
+    w = '0' * (64 - len(w)) + w
+    b = bin(b & p)[2:]
+    b = '0' * (64 - len(b)) + b
+
+    # conjoin. white + black -
+    out = []
+    for white, black, in zip(w, b):
+        out.append(int(white) - int(black))
+    return out
+
+
+# creates an input tensor
 def get_x(node):
-    """
-    sample input:
-    8/8/p3k3/P2p4/1p1p3p/1P1Pq2P/5N2/5R1K b - - 10 58
-    """
-    board_dict = {
-        '1': [[0, 0, 0, 0, 0, 0]],
-        '2': [[0, 0, 0, 0, 0, 0] for _ in range(2)],
-        '3': [[0, 0, 0, 0, 0, 0] for _ in range(3)],
-        '4': [[0, 0, 0, 0, 0, 0] for _ in range(4)],
-        '5': [[0, 0, 0, 0, 0, 0] for _ in range(5)],
-        '6': [[0, 0, 0, 0, 0, 0] for _ in range(6)],
-        '7': [[0, 0, 0, 0, 0, 0] for _ in range(7)],
-        '8': [[0, 0, 0, 0, 0, 0] for _ in range(8)],
-        'P': [[1, 0, 0, 0, 0, 0]],
-        'N': [[0, 1, 0, 0, 0, 0]],
-        'B': [[0, 0, 1, 0, 0, 0]],
-        'R': [[0, 0, 0, 1, 0, 0]],
-        'Q': [[0, 0, 0, 0, 1, 0]],
-        'K': [[0, 0, 0, 0, 0, 1]],
-        'p': [[-1, 0, 0, 0, 0, 0]],
-        'n': [[0, -1, 0, 0, 0, 0]],
-        'b': [[0, 0, -1, 0, 0, 0]],
-        'r': [[0, 0, 0, -1, 0, 0]],
-        'q': [[0, 0, 0, 0, -1, 0]],
-        'k': [[0, 0, 0, 0, 0, -1]],
-    }
-    castle_dict = {
-        'k': [[]],
-        'q': [[]],
-        'K': [[]],
-        'Q': [[]]
-    }
+    board = node.board()
+    turn = board.turn
+    castling = board.castling_rights
+    enpassant = board.ep_square
+    halfmove = board.halfmove_clock
+    fullmove = board.fullmove_number
+    white, black = board.occupied_co
 
-    fen = node.board().fen()
-    board, turn, castle, enpassant, halfmove, fullmove = str(fen).split()
+    out = [to_bin_array(board.pawns, white, black),
+           to_bin_array(board.knights, white, black),
+           to_bin_array(board.bishops, white, black),
+           to_bin_array(board.rooks, white, black),
+           to_bin_array(board.queens, white, black),
+           to_bin_array(board.kings, white, black)]
 
-    board = board.split('/')
-    # print(board)
-    output = []
-    for row in board:
-        for square in row:
-            output += board_dict[square]
+    # print(out[0])
 
-    # output = np.reshape(output, (8, 8, 6))  # 64x6 -> 8x8x6
-    # if turn == 'b':                         # if black to move, flip board vertically
-    #     output = np.flip(output, 0)
-
-    # pprint.pprint(output)
-    return output
+    return out
